309c422326cd602b1323a63e10fdde7fc0c9116f
Julien Guery <julien.guery@telecom-bretagne.eu>
2013-12-30 23:43:16 +0100

    Added a typing error

--- Main/Errors.ml
+++ Main/Errors.ml
@@ -2,8 +2,10 @@ open Location
 
 type error = 
 	| SyntaxError
+	| TypeError of string * string
 
 exception PError of error * Location.t
 
 let string_of_error = function
 	| SyntaxError -> "Syntax error"
+	| TypeError(exp, real) -> "Type error. Expected type" ^ exp ^ " , but got type " ^ real
--- Typing/Typer.ml
+++ Typing/Typer.ml
@@ -1,5 +1,6 @@
 open Located
 open Location
+open Errors
 open Structure
 
 type exprType = 
@@ -12,11 +13,33 @@ let string_of_expr_type = function
 	| BooleanType -> "Boolean"
 	| StringType -> "String"
 
-let rec find_expr_type = function
-	| Int(i) -> IntType
-	| Boolean(b) -> BooleanType
-	| String(s) -> StringType
+let make_error expr exp_type real_type = 
+	raise (PError(TypeError(string_of_expr_type exp_type, string_of_expr_type real_type), 
+		Located.loc_of expr))
+
+let rec type_of_expr exp = 
+	let rec type_of_unop unop locExpr = match unop with
+		| Udiff -> (match (type_of_expr (Located.elem_of locExpr)) with
+			| BooleanType -> BooleanType
+			| _ as t -> make_error locExpr BooleanType t
+		)
+	  	| Uminus -> (match (type_of_expr (Located.elem_of locExpr)) with
+	  		| IntType -> IntType
+	  		| _ as t -> make_error locExpr IntType t
+	  	)
+  	in match exp with 
+		| Int(i) -> IntType
+		| Boolean(b) -> BooleanType
+		| String(s) -> StringType
+		| Unop(u, e) -> type_of_unop (Located.elem_of u) e
+
+let rec type_of_structure_tree tree = 
+	let rec type_of_structure = function
+		| Expr e -> type_of_expr (Located.elem_of e)
+	in match tree with 
+	| t::q -> type_of_structure t
 
 let _ = 
-	let expr = Int(Located.mk_elem 1 Location.none) in
-	print_string (string_of_expr_type (find_expr_type expr))
+	let expr = String(Located.mk_elem "true" Location.none) in
+		print_string (string_of_expr_type (type_of_expr expr));
+		print_newline()
